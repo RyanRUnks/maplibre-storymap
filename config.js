d6966d34dbdd594cfa37b3d40d4ecb163b74c56d
RyanRUnks <ryanrunks@gmail.com>
2025-07-10 14:11:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ var config = {
             legend: '<span style="font-size: 0.85em;"><em>Legend content</em></span>',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: [[37.25098, -2.40363]],
+                center: [37.25098, -2.40363],
                 zoom: 9.41,
                 pitch: 0,
                 bearing: 0
