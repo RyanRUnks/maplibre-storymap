52a46ef676045df7286d78afadececced5957cd3
RyanRUnks <ryanrunks@gmail.com>
2025-08-20 03:31:47 +0200

    big update

--- config.js
+++ config.js
@@ -12,10 +12,122 @@ var config = {
     title: 'Ujamaa Community Resource Team (UCRT)',
     logo: '',
     subtitle: 'Empowering pastoralist, agro-pastoralist and hunter-gatherer communities',
-    byline: 'By a Digital Storyteller',
     mobileview: '<div id="rotate-mobile"><em>For optimal viewing of this storytelling map on mobile, rotate your device to a horizontal orientation.</em><br><br><img src="images/device.png">', // to add custom messaging in the header for mobile devices
     footer: 'Source: source citations, etc.<br> Created using <a href="https://github.com/digidem/maplibre-storymap" target="_blank">MapLibre Storytelling</a> template.',
     chapters: [
+                {
+            id: 'chapter-id2',
+            alignment: 'left',
+            hidden: false,
+            title: 'Districts UCRT works in',
+            image: './images/landscape.png',
+            caption: '',
+            website: '',
+            author: 'UCRT',
+            legend: '<span style="font-size: 0.85em;"><em>Legend content</em></span>',
+            description: 'Strengthening tenure arrangements beyond CVLs through communal Certificates of Customary Right of Occupancy (CCROs) that institutionalise collective tenure arrangements for pastoralists and hunter-gatherers. Established Women’s Rights and Leadership Forums (WRLFs) to promote women’s rights to own, and make decisions over, land and property.  ',
+            location: {
+                center: [35, -4.05],
+                zoom: 7.25,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: true,
+            callback: '',
+            onChapterEnter: [
+                {layer: 'my-geojson-fill',opacity: 1},
+                {layer: 'my-geojson-fill2',opacity: 1}
+            ],
+            onChapterExit: [
+                 {layer: 'my-geojson-fill',opacity: 1},
+                 {layer: 'my-geojson-fill2',opacity: 1}
+            ]
+        },
+
+        {
+            id: 'chapter-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Districts UCRT works in',
+            image: './images/pastoralism.png',
+            caption: '',
+            website: '',
+            author: 'UCRT',
+            legend: '<span style="font-size: 0.85em;"><em>Legend content</em></span>',
+            description: 'Strengthening tenure arrangements beyond CVLs through communal Certificates of Customary Right of Occupancy (CCROs) that institutionalise collective tenure arrangements for pastoralists and hunter-gatherers. Established Women’s Rights and Leadership Forums (WRLFs) to promote women’s rights to own, and make decisions over, land and property.  ',
+            location: {
+                center: [35, -4.05],
+                zoom: 7.25,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: true,
+            callback: '',
+            onChapterEnter: [
+                 {
+                      layer: 'my-geojson-fill',
+                      opacity: 0
+                 },
+                 {
+                      layer: 'my-geojson-fill2',
+                      opacity: 0.5
+                 }
+            ],
+            onChapterExit: [
+                 {
+                      layer: 'my-geojson-fill',
+                      opacity: 0
+                 },
+                 {
+                      layer: 'my-geojson-fill2',
+                      opacity: 0
+                 }
+            ]
+        },
+                        {
+            id: 'other-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Second Title',
+            image: './images/hadzabe.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [35, -4.05],
+                zoom: 5,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            mapInteractive: false,
+            callback: '',
+            onChapterEnter: [
+                 {
+                      layer: 'my-geojson-fill',
+                      opacity: 0
+                 },
+                 {
+                      layer: 'my-geojson-fill2',
+                      opacity: 0
+                 }
+            ],
+            onChapterExit: [
+                 {
+                      layer: 'my-geojson-fill',
+                      opacity: 0
+                 },
+                 {
+                      layer: 'my-geojson-fill2',
+                      opacity: 0
+                 }
+            ]
+        },
         {
             id: 'my-first-chapter',
             alignment: 'left',
@@ -28,8 +140,8 @@ var config = {
             legend: '<span style="font-size: 0.85em;"><em>Legend content</em></span>',
             description: 'Strengthening tenure arrangements beyond CVLs through communal Certificates of Customary Right of Occupancy (CCROs) that institutionalise collective tenure arrangements for pastoralists and hunter-gatherers. Established Women’s Rights and Leadership Forums (WRLFs) to promote women’s rights to own, and make decisions over, land and property.  ',
             location: {
-                center: [36.7672, -5.2448],
-                zoom: 9.41,
+                center: [36.12, -3.8],
+                zoom: 7.71,
                 pitch: 0,
                 bearing: 0
             },
@@ -39,72 +151,26 @@ var config = {
             mapInteractive: true,
             callback: '',
             onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
-                // {
-                //      layer: 'countries-fill',
-                //      opacity: 1
-                // }
+                 {
+                      layer: 'my-geojson-fill',
+                      opacity: 1
+                 },
+                 {
+                      layer: 'my-geojson-fill2',
+                      opacity: 0
+                 }
             ],
             onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
+                 {
+                      layer: 'my-geojson-fill',
+                      opacity: 0
+                 },
+                 {
+                      layer: 'my-geojson-fill2',
+                      opacity: 0
+                 }
             ]
         },
-        {
-            id: 'other-identifier',
-            alignment: 'right',
-            hidden: false,
-            title: 'Second Title',
-            image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
-            location: {
-                center: [-79, 0],
-                zoom: 5,
-                pitch: 0,
-                bearing: -90
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            mapInteractive: false,
-            callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: 'countries-fill',
-                //     opacity: 0
-                // }
-            ],
-            onChapterExit: []
-        },
-        {
-            id: 'chapter-id',
-            alignment: 'left',
-            hidden: false,
-            title: 'Chapter title',
-            image: './images/name.png',
-            caption: 'Source image caption',
-            website: '<a href="" target="_blank">Website</a>',
-            author: 'Digital Storyteller',
-            legend: '<span style="font-size: 0.85em;"><em>Legend content</em></span>',
-            description: 'Your chapter content',
-            location: {
-                center: [-75.15911, 39.97307],
-                zoom: 10.89,
-                pitch: 0,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            spinGlobe: false,
-            mapInteractive: true,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
+
     ]
 };
--- sources.js
+++ sources.js
@@ -1,20 +1,34 @@
-var customSourcesAndLayers = {
+var externalData = {
     sources: [
-//      {
-//          "id": "my-geojson-source", 
-//          "type": "geojson",
-//          "url": "map/CCRO.geojson"
-//        },
+        {
+            "name": "my-geojson-source", // Use "name", not "id"
+            "type": "geojson",
+            "data": "./map/CCRO.geojson"
+        },
+        {
+            "name": "my-geojson-source2", // Use "name", not "id"
+            "type": "geojson",
+            "data": "./map/UCRT_districts.geojson"
+        }
     ],
     layers: [
-//      {
-//          "id": "my-geojson-fill",
-//          "type": "fill",
-//          "source": "my-geojson-source",
-//          "paint": {
-//              "fill-color": "#B42222",
-//              "fill-opacity": 0.5
-//          }
-//      }
+        {
+            "id": "my-geojson-fill",
+            "type": "fill",
+            "source": "my-geojson-source",
+            "paint": {
+                "fill-color": "#B42222",
+                "fill-opacity": 0.5
+            }
+        },
+        {
+            "id": "my-geojson-fill2",
+            "type": "fill",
+            "source": "my-geojson-source2",
+            "paint": {
+                "fill-color": "#B42222",
+                "fill-opacity": 0.5
+            }
+        }
     ]
 };
